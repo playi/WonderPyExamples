0283048f5a8655dc390a2944b3480ee6f20e4047
Orion Elenzil <dococelot@gmail.com>
2018-07-30 14:52:51 -0700

    #1: simpler code

--- tutorial/01_hello_world.py
+++ tutorial/01_hello_world.py
@@ -7,24 +7,8 @@ from WonderPy.components.wwMedia import WWMedia
 
 
 """
-This example shows very basic connecting to a robot and sending some simple commands.
-Basic steps:
-
-1. add the imports
-2. create a Class (in this case named "MyClass").
-3. create a method named "on_connect" which accepts self and a robot parameter.
-     eg on_connect(self, robot).
-     This will be called when the program connects to a robot.
-4. kick things off my passing an instance of your Class to WonderPy.wwMain.start():
-     WonderPy.wwMain.start(MyClass())
-5. Try it! Your on_connect method should be called.
-6. on_connect() itself should not block - ie, it should return control as soon as possible.
-     However, on_connect() can launch some asynchronous processes, which can block.
-     So do that. In this example we spawn a thread on method thread_hello().
-7. In the thread, try out some robot commands!
-     * Commands of the flavour "stage_foo()" simply send the command to the robot and return.
-         ie, they do not block.
-     * Commands of the flavour "do_foo()" send the commands and block until the command completes.
+This example shows connecting to the robot and issuing some simple commands.
+See the other 'tutorial' and 'misc' examples for more complex scenarios!
 """
 
 
@@ -39,38 +23,51 @@ class MyClass(object):
         Thread(target=self.thread_hello, args=(robot,)).start()
 
     def thread_hello(self, robot):
-        """
-        :param robot: WWRobot
-        """
+        # robot is the robot we've connected to.
 
-        # dictionary mapping robot types to a few sounds for that robot
-        hello_sounds = {
-            WWRobotConstants.RobotType.WW_ROBOT_DASH : [WWMedia.WWSound.WWSoundDash.HOWDY,
-                                                        WWMedia.WWSound.WWSoundDash.HOWSGOING       ],
-            WWRobotConstants.RobotType.WW_ROBOT_DOT  : [WWMedia.WWSound.WWSoundDot .HOWDY,
-                                                        WWMedia.WWSound.WWSoundDot .HOLD_ME         ],
-            WWRobotConstants.RobotType.WW_ROBOT_CUE  : [WWMedia.WWSound.WWSoundCue .zest_HEYWHSU,
-                                                        WWMedia.WWSound.WWSoundCue .charge_BORESTNOC],
-        }
+        sound_names = self.get_hello_sounds(robot)
 
-        if robot.robot_type not in hello_sounds:
-            raise ValueError("unhandled robot type: %s on %s" % (str(robot.robot_type), robot.name))
-
-        for sound_name in hello_sounds[robot.robot_type]:
-
+        for sound_name in sound_names:
+            # for each sound in the list: turn on the lights, play the sound, dim the lights, then pause a little.
             print("On %s, setting all RGB lights to white." % (robot.name))
             robot.cmds.RGB.stage_all(1, 1, 1)
 
             print("On %s, playing '%s'." % (robot.name, sound_name))
             robot.cmds.media.do_audio(sound_name)
 
-            print("On %s, setting all RGB lights to off." % (robot.name))
-            robot.cmds.RGB.stage_all(0, 0, 0)
+            print("On %s, setting all RGB lights to dim." % (robot.name))
+            robot.cmds.RGB.stage_all(0.2, 0.2, 0.2)
 
             print("Waiting a little bit.")
-            time.sleep(1)
+            time.sleep(3)
 
+        print("On %s, setting all RGB lights to off." % (robot.name))
+        robot.cmds.RGB.stage_all(0, 0, 0)
         print("That's all for now.")
+
+    def get_hello_sounds(self, for_this_robot):
+        # we don't know ahead-of-time if the robot will be a Cue, Dash, or Dot,
+        # so we handle each of those cases now with an appropriate list of sounds to play:
+        if for_this_robot.robot_type == WWRobotConstants.RobotType.WW_ROBOT_DASH:
+            return [WWMedia.WWSound.WWSoundDash.HOWDY,
+                    WWMedia.WWSound.WWSoundDash.HOWSGOING,
+                    WWMedia.WWSound.WWSoundDash.LETS_GO,
+                    ]
+
+        elif for_this_robot.robot_type == WWRobotConstants.RobotType.WW_ROBOT_DOT:
+            return [WWMedia.WWSound.WWSoundDot.HOWDY,
+                    WWMedia.WWSound.WWSoundDot.HOLD_ME,
+                    WWMedia.WWSound.WWSoundDot.READYSET,
+                    ]
+
+        elif for_this_robot.robot_type == WWRobotConstants.RobotType.WW_ROBOT_CUE:
+            return [WWMedia.WWSound.WWSoundCue.zest_HEYWHSU,
+                    WWMedia.WWSound.WWSoundCue.charge_BORESTNOC,
+                    WWMedia.WWSound.WWSoundCue.pep_YOUSOGOT,
+                    ]
+        else:
+            raise ValueError("unhandled robot type: %s on %s" % (str(for_this_robot.robot_type), for_this_robot.name))
+
 
 
 # kick off the program !
